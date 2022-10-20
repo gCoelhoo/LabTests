5186ff4b95a529eb13d5509898aed0f336b80649
Gabriel Coelho <up201902223@up.pt>
2022-10-20 15:37:45 +0100

    Step 9. adding pitest config to build.gradle

--- src/test/java/com/aor/numbers/ListDeduplicatorTest.java
+++ src/test/java/com/aor/numbers/ListDeduplicatorTest.java
@@ -31,9 +31,10 @@ public class ListDeduplicatorTest {
         List<Integer> list = Arrays.asList(1,2,4,2);
         List<Integer> expected = Arrays.asList(1,2,4);
 
-        GenericListDeduplicator deduplicator = Mockito.mock(GenericListDeduplicator.class);
-        Mockito.when(deduplicator.deduplicate(list)).thenReturn(Arrays.asList(1,2,4));
+        GenericListSorter sorter = Mockito.mock(GenericListSorter.class);
+        Mockito.when(sorter.sort(Mockito.anyList()) ).thenReturn(Arrays.asList(1,2,2,4));
 
+        ListDeduplicator deduplicator = new ListDeduplicator(sorter);
         List<Integer> distinct = deduplicator.deduplicate(list);
 
         Assertions.assertEquals(expected, distinct);
